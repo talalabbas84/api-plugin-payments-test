5d59a2b33a3ede0711b6d5a6881697c44d0e2218
Talal Abbas <talalabbas84@gmail.com>
2021-07-16 17:40:26 +0500

    DASDAS

---
+++ src/queries/index.js
@@ -0,0 +1,9 @@
+import availablePaymentMethodsTest from "./availablePaymentMethods.js";
+import getPaymentMethodConfigByName from "./getPaymentMethodConfigByName.js";
+import paymentMethods from "./paymentMethods.js";
+
+export default {
+  availablePaymentMethodsTest,
+  getPaymentMethodConfigByName,
+  paymentMethods
+};
